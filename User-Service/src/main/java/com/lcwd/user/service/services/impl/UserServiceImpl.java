1392ff44450d9d7fb9e3d57b21ac1bea6201aa3a
Manish Kumar <manishkumar10071998@gmail.com>
2024-02-09 14:41:44 +0530

    Enstablish communication between user-service and rating-service

--- User-Service/src/main/java/com/lcwd/user/service/services/impl/UserServiceImpl.java
+++ User-Service/src/main/java/com/lcwd/user/service/services/impl/UserServiceImpl.java
@@ -1,18 +1,23 @@
 package com.lcwd.user.service.services.impl;
 
+import com.lcwd.user.service.entities.Rating;
 import com.lcwd.user.service.entities.User;
 import com.lcwd.user.service.exceptions.ResourceNotFoundException;
 import com.lcwd.user.service.repositories.UserRepository;
 import com.lcwd.user.service.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private RestTemplate restTemplate;
     @Override
     public User saveUser(User user) {
         return userRepository.save(user);
@@ -25,6 +30,10 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUser(String userId) {
-        return userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("Not Found"+userId));
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("Not Found"+userId));
+        //fetch rating for the user from rating-Service
+        ArrayList<Rating> userRatings = restTemplate.getForObject("http://localhost:8083/ratings/hotels/"+user.getUserId(), ArrayList.class);
+        user.setRatings(userRatings);
+        return user;
     }
 }
